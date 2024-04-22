984cc9a7c43869529f91ad3d6a40eba610bd311c
Nguyễn Trúc An <114329768+nguyentrucan@users.noreply.github.com>
2024-04-23 02:15:17 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login';
+import Resetpassword from './pages/Resetpassword';
+import Forgotpassword from './pages/Forgotpassword';
+import MainLayout from './components/MainLayout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/reset-password' element={<Resetpassword />} />
+        <Route path='/forgot-password' element={<Forgotpassword />} />
+        <Route path='/admin' element={<MainLayout />}>
+          <Route index element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Resetpassword.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Resetpassword = () => {
+  return (
+    <div>
+      Resetpassword
+    </div>
+  )
+}
+
+export default Resetpassword
